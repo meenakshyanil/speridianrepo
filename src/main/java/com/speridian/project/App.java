edac038e7a1a0cbb24032cfa3a2ffb13be80cf87
Meenakshyanil <meenakshy.anil.nair@gmail.com>
2023-10-18 14:55:14 +0530

    First Commit

---
+++ src/main/java/com/speridian/project/App.java
@@ -0,0 +1,17 @@
+package com.speridian.project;
+
+import java.util.TreeSet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	TreeSet<String> s = new TreeSet<>(); s.add(null);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
